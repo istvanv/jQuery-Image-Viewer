72b3f6a347f98e30639027b4a89ab11ef00138e6
Istvan Vincze <istvan@vincze.biz>
2011-07-22 17:09:03 +1000

    implementing navigations

--- imageviewer.jquery.js
+++ imageviewer.jquery.js
@@ -15,43 +15,81 @@
 
     // Private
     var imageTransition = function(args) {
-
+        var $this = args.el;
+        var settings = $this.data('imageViewer');
+        
         if ($.browser.msie) {
-            if (args['action'] == 'show') args['targetEl'].show();
-            else if (args['action'] == 'hideremove') args['targetEl'].remove();
-            else args['targetEl'].hide();
-            if ($.isFunction(args['callbackFn'])) args['callbackFn'].call(this);
+            if (args.action == 'show') args.targetEl.show();
+            else if (args.action == 'hideremove') args.targetEl.remove();
+            else args.targetEl.hide();
+            if ($.isFunction(args.callbackFn)) args.callbackFn.call(this);
         } else {
-            if (args['action'] == 'show') {
-                args['targetEl'].fadeIn(300, function() {
-                    if ($.isFunction(args['callbackFn'])) args['callbackFn'].call(this);
+            if (args.action == 'show') {
+                args.targetEl.fadeIn(300, function() {
+                    if ($.isFunction(args.callbackFn)) args.callbackFn.call(this);
                 });
-            } else if (args['action'] == 'hideremove') {
-                args['targetEl'].fadeOut(200, function() {
-                    args['targetEl'].remove();
-                    if ($.isFunction(args['callbackFn'])) args['callbackFn'].call(this);
+            } else if (args.action == 'hideremove') {
+                args.targetEl.fadeOut(200, function() {
+                    args.targetEl.remove();
+                    if ($.isFunction(args.callbackFn)) args.callbackFn.call(this);
                 });
             } else {
-                args['targetEl'].fadeOut(200, function() {
-                    if ($.isFunction(args['callbackFn'])) args['callbackFn'].call(this);
+                args.targetEl.fadeOut(200, function() {
+                    if ($.isFunction(args.callbackFn)) args.callbackFn.call(this);
                 });
             }
         }
 
     };
+    
 
 
     var setAsSelected = function(args) {
-        args['selectorThumbs'].parents('li').removeClass('selected');
-        args['thumbEl'].parents('li').addClass('selected');
+        var $this = args.el;
+        var settings = $this.data('imageViewer');
+        
+        args.selectorThumbs.parents('li').removeClass('selected');
+        args.thumbEl.parents('li').addClass('selected');
     };
+
+
+
+    var switchImage = function(args) {
+        var $this = args.el;
+        var settings = $this.data('imageViewer');
+        
+        settings.largeImageWrapper.css({ height : $('img:first', settings.largeImageWrapper).innerHeight() }); // So the imageWrapper doesn't collapse when image is removed.
+
+        $(new Image())
+            .load(function() {
+                var newImage = this;
+                $(newImage).hide();
+                settings.largeImageWrapper.append(newImage);
+
+                imageTransition({
+                    el : $this,
+                    targetEl : $('img:first', settings.largeImageWrapper),
+                    action : 'hideremove',
+                    callbackFn : function() {
+                        var nWidth = $(newImage).innerWidth();
+                        var nHeight = $(newImage).innerHeight();
+                        if (settings.largeImageWrapper.innerHeight() != nHeight) settings.largeImageWrapper.animate({ 'height' : nHeight }, 350, function() { imageTransition({ el : $this, targetEl : $(newImage), action : 'show' }); });
+                        else imageTransition({ el : $this, targetEl : $(newImage), action : 'show' });
+                    } });
+                
+            })
+            .attr({
+                src : args.largeImageSrc,
+                alt : args.largeImageAlt
+            });
+    };
+    
 
 
     // Public
     var methods = {
         
         init : function(options) {
-
             return this.each(function() {
 
                 var settings = {
@@ -61,52 +99,166 @@
 
                 if (options) $.extend(settings, options);
 
-                var $this               = $(this);
-                var $largeImageWrapper  = $(settings.largeImageWrapper, this);
-                var $imageThumbs        = $(settings.imageThumbs, this);
-                var $firstThumb         = $imageThumbs.first();
+                var $this                   = $(this);
+                settings.largeImageWrapper  = $(settings.largeImageWrapper, this);
+                settings.imageThumbs        = $(settings.imageThumbs, this);
+                var $firstThumb             = settings.imageThumbs.first();
+                
+                $this.data('imageViewer', settings);
+                
+                // Attach event handling to control links
+                $this.find('.next, .previous, .first, .last').bind('click.imageViewer', function(e) {
+                    e.preventDefault();
 
-                setAsSelected({ selectorThumbs : $imageThumbs, thumbEl : $firstThumb });
+                    var action = $(this).attr('class');
+                    
+                    //console.log('action: '+action);
+                    
+                    if (action == 'next') methods.loadNext.call($this);
+                    if (action == 'previous') methods.loadPrevious.call($this);
+                    if (action == 'first') methods.loadFirst.call($this);
+                    if (action == 'last') methods.loadLast.call($this);
+                });
 
-                if ($largeImageWrapper.is(':empty')) {
-                    $largeImageWrapper.append('<img src="'+$firstThumb.parent('a').attr('href')+'" alt="'+$firstThumb.attr('alt')+'">');
-                    //$firstThumb.parent('a').focus();
+
+                // load first image
+                if (settings.largeImageWrapper.is(':empty')) {
+                    settings.largeImageWrapper.append('<img src="" alt="">');
                 }
+                methods.loadFirst.call($this);
 
-                $imageThumbs.parent('a').bind('click.imageViewer focus.imageViewer', function(e) {
+
+                // Attach event handling to thumbnails
+                settings.imageThumbs.parent('a').bind('click.imageViewer focus.imageViewer', function(e) {
 
                     e.preventDefault();
- 
                     var $thumbEl = $(this).find('img');
                     if ($thumbEl.parents('li').hasClass('selected')) return false;
 
-                    setAsSelected({ selectorThumbs : $imageThumbs, thumbEl : $thumbEl });
-                    $largeImageWrapper.css({ height : $('img:first', $largeImageWrapper).innerHeight() }); // So the imageWrapper doesn't collapse when image is removed.
+                    setAsSelected({
+                        el : $this,
+                        selectorThumbs : settings.imageThumbs,
+                        thumbEl : $thumbEl
+                    });
 
-                    var $largeImageSrc  = $thumbEl.parent('a').attr('href');
-                    var largeImageAlt   = $thumbEl.attr('alt');
-                    largeImageAlt       = (largeImageAlt == '') ? $largeImageSrc.split('/').pop() : largeImageAlt;
-
-                    $(new Image())
-                        .load(function() {
-                            var newImage = this;
-                            $(newImage).hide();
-                            $largeImageWrapper.append(newImage);
-            
-                            imageTransition({ targetEl : $('img:first', $largeImageWrapper), action : 'hideremove', callbackFn : function() {
-                                var nWidth = $(newImage).innerWidth();
-                                var nHeight = $(newImage).innerHeight();
-                                if ($largeImageWrapper.innerHeight() != nHeight) $largeImageWrapper.animate({ 'height' : nHeight }, 350, function() { imageTransition({ targetEl : $(newImage), action : 'show' }); });
-                                else imageTransition({ targetEl : $(newImage), action : 'show' });
-                            } });
-                            
-                        })
-                        .attr({ src : $largeImageSrc, alt : largeImageAlt });
-    
+                    switchImage({
+                        el : $this,
+                        largeImageSrc : $thumbEl.parent('a').attr('href'),
+                        largeImageAlt : ($thumbEl.attr('alt') == '') ? $thumbEl.attr('alt') : ''
+                    });
+                    
+                    $this.data('imageViewer').current = $thumbEl;
                 });
 
             });
+        },
 
+
+        loadNext : function() {
+            return this.each(function() {
+                var $this = $(this), settings = $this.data('imageViewer');
+                
+                var theNext = $(settings.current, settings.imageThumbs).index() + 1;
+                
+                var $thumbEl = settings.imageThumbs[theNext];
+
+                console.log('$thumbEl: ' + $thumbEl.attr('src'));
+
+                //var $thumbEl = settings.imageThumbs[settings.current].next();
+                
+
+                
+                if (!$thumbEl && settings.cycle) {
+                    methods.loadFirst.call($this);
+                }
+
+                setAsSelected({
+                    el : $this,
+                    selectorThumbs : settings.imageThumbs,
+                    thumbEl : $thumbEl
+                });
+    
+                switchImage({
+                    el : $this,
+                    largeImageSrc : $thumbEl.parent('a').attr('href'),
+                    largeImageAlt : ($thumbEl.attr('alt') == '') ? $thumbEl.attr('alt') : ''
+                });
+                
+                $this.data('imageViewer').current = $thumbEl;
+            });            
+        },
+
+
+        loadPrevious : function() {
+            return this.each(function() {
+                var $this = $(this), settings = $this.data('imageViewer');
+
+                var $thumbEl = settings.imageThumbs[settings.current].prev();
+
+                if (!$thumbEl && settings.cycle) {
+                    methods.loadLast;
+                }
+
+                setAsSelected({
+                    el : $this,
+                    selectorThumbs : settings.imageThumbs,
+                    thumbEl : $thumbEl
+                });
+    
+                switchImage({
+                    el : $this,
+                    largeImageSrc : $thumbEl.parent('a').attr('href'),
+                    largeImageAlt : ($thumbEl.attr('alt') == '') ? $thumbEl.attr('alt') : ''
+                });
+                
+                $this.data('imageViewer').current = $thumbEl;
+            });
+        },
+
+
+        loadFirst : function() {
+            return this.each(function() {
+                var $this = $(this), settings = $this.data('imageViewer'); 
+
+                var $thumbEl = settings.imageThumbs.first();
+
+                setAsSelected({
+                    el : $this,
+                    selectorThumbs : settings.imageThumbs,
+                    thumbEl : $thumbEl
+                });
+    
+                switchImage({
+                    el : $this,
+                    largeImageSrc : $thumbEl.parent('a').attr('href'),
+                    largeImageAlt : ($thumbEl.attr('alt') == '') ? $thumbEl.attr('alt') : ''
+                });
+                
+                $this.data('imageViewer').current = $thumbEl;
+            });
+        },
+
+
+        loadLast : function() {
+            return this.each(function() {
+                var $this = $(this), settings = $this.data('imageViewer');
+
+                var $thumbEl = settings.imageThumbs.last();
+
+                setAsSelected({
+                    el : $this,
+                    selectorThumbs : settings.imageThumbs,
+                    thumbEl : $thumbEl
+                });
+    
+                switchImage({
+                    el : $this,
+                    largeImageSrc : $thumbEl.parent('a').attr('href'),
+                    largeImageAlt : ($thumbEl.attr('alt') == '') ? $thumbEl.attr('alt') : ''
+                });
+                
+                $this.data('imageViewer').current = $thumbEl;
+            });
         }
 
     };
